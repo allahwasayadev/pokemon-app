8f21cd6e9c0a52be329d5efaf23646b21c864dc4
allahwasayadev <allahwasaya11799@gmail.com>
2024-06-03 22:35:39 -0400

    Added Jest configuration for test coverage reporting (JSON & HTML)

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['./jest.setup.js'],
+  collectCoverage: true,
+  coverageReporters: ["json", "html"],
 };
